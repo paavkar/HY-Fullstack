f4a80cb2da50044240175c608934ba27229a0201
paavkar <62183220+paavkar@users.noreply.github.com>
2022-02-24 19:31:27 +0200

    Changed App.js and index.js
    Changed the files to match the tasks

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = (props) => {
+	return (
+	 <div>
+	  <h1> {props.course} </h1>
+	 </div>
+	)
+}
 
-function App() {
+const Part = (props) => {
+	return (
+	 <div>
+	  <p> {props.part1} {props.exercises1} </p>
+	  <p> {props.part2} {props.exercises2} </p>
+	  <p> {props.part3} {props.exercises3} </p>
+	 </div>
+	)
+}
+
+const Content = (props) => {
+	return (
+	 <div>
+	  <Part part1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
+	  <Part part2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
+	  <Part part3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
+	 </div>
+	)
+}
+
+const Total = (props) => {
+	return (
+	<>
+	 <h4>Number of exercises {props.total} </h4>
+	</>
+	)
+}
+
+const App = () => {
+  const course = { 
+  	name: 'Half Stack application development',
+  	parts: [
+    	{
+ 	   		name: 'Fundamentals of React',
+      	exercises: 10 
+    	},
+    	{
+      	name: 'Using props to pass data',
+      	exercises: 7
+    	},
+    	{
+      	name: 'State of a component',
+      	exercises: 14
+    	}
+   	]	
+	}
+	let alku = 0
+	const total = course.parts.reduce( (s, p) => s+p.exercises, alku)
+	
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+	  	<Total parts={course.parts} total={total}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,70 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const Header = (props) => {
-	return (
-	 <div>
-	  <h1> {props.course} </h1>
-	 </div>
-	)
-}
-
-const Part = (props) => {
-	return (
-	 <div>
-	  <p> {props.part1} {props.exercises1} </p>
-	  <p> {props.part2} {props.exercises2} </p>
-	  <p> {props.part3} {props.exercises3} </p>
-	 </div>
-	)
-}
-
-const Content = (props) => {
-	return (
-	 <div>
-	  <Part part1={props.parts[0].name} exercises1={props.parts[0].exercises}/>
-	  <Part part2={props.parts[1].name} exercises2={props.parts[1].exercises}/>
-	  <Part part3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
-	 </div>
-	)
-}
-
-const Total = (props) => {
-	return (
-	<>
-	 <h4>Number of exercises {props.total} </h4>
-	</>
-	)
-}
-
-const App = () => {
-  const course = { 
-  	name: 'Half Stack application development',
-  	parts: [
-    	{
- 	   		name: 'Fundamentals of React',
-      	exercises: 10 
-    	},
-    	{
-      	name: 'Using props to pass data',
-      	exercises: 7
-    	},
-    	{
-      	name: 'State of a component',
-      	exercises: 14
-    	}
-   	]	
-	}
-	let alku = 0
-	const total = course.parts.reduce( (s, p) => s+p.exercises, alku)
-	
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-	  	<Total parts={course.parts} total={total}/>
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
